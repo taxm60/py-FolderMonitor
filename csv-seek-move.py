a84e86bb3778a8cfdda749fbda9025d7d396cc5e
taxm60 <157100645+taxm60@users.noreply.github.com>
2024-07-24 22:17:41 +0800

    Update csv-seek-move.py
    Monitor with prefix "f" and end with filetype ".ods" and ".csv", also add exception handler

--- csv-seek-move.py
+++ csv-seek-move.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import time
+import getpass   #get current username
 
 def move_csv_files(source_folder, destination_folder):
     # Check if destination folder exists, create if it doesn't
@@ -12,23 +13,33 @@ def move_csv_files(source_folder, destination_folder):
     
     # Iterate through each file
     for file in files:
-        if file.endswith('.csv'):
-            # Construct full paths for source and destination
+        # filename is like  (f***.csv) OR (f***.ods)
+        if file.startswith('f') and (file.endswith('.csv') or file.endswith('.ods')):
+            # Construct full paths for source and destination            
             source_file = os.path.join(source_folder, file)
             destination_file = os.path.join(destination_folder, file)
             
-            # Move the file to the destination folder
-            shutil.move(source_file, destination_file)
-            print(f"Moved {file} to {destination_folder}")
+            # Move the file to the destination folde
+            try:
+                shutil.move(source_file, destination_file)
+                #print(f"[O] Moving: {file} -> {destination_folder}")
+                print(f"\033[92m[O] Moving: {file} -> {destination_folder}\033[0m")  # Green color for success
+            except PermissionError:
+                #print(f"[X] PermissionErr: Moving {file} ; keep retry...")
+                print(f"\033[91;1m[X] PermissionErr: Moving {file} ; keep retry...\033[0m")  # Light red color for PermissionError
+                
+            except Exception as e:
+                print(f"[X] Unexpected error: {e}")
 
 def main():
-    source_folder = r"C:\Users\jack\Downloads"
+    username = getpass.getuser()
+    source_folder = os.path.join(r"C:\Users", username, "Downloads")
     destination_folder = os.path.join(source_folder, "done")
     
     while True:
         move_csv_files(source_folder, destination_folder)
         # Check every 2 seconds (adjust as needed)
-        time.sleep(2)
+        time.sleep(3)
 
 if __name__ == "__main__":
     main()
