ce35487e532e157a61f22b29365edd7afa968212
taxm60 <157100645+taxm60@users.noreply.github.com>
2024-07-29 17:08:38 +0800

    Update csv-seek-move.py

--- csv-seek-move.py
+++ csv-seek-move.py
@@ -9,12 +9,13 @@ def move_csv_files(source_folder, destination_folder):
         os.makedirs(destination_folder)
     
     # List all files in the source folder
-    files = os.listdir(source_folder)
+    x     = os.listdir(source_folder)
+    files = [file.lower() for file in x]
     
     # Iterate through each file
     for file in files:
         # filename is like  (f***.csv) OR (f***.ods)
-        if file.startswith('f') and (file.endswith('.csv') or file.endswith('.ods')):
+        if file.startswith('f11') and (file.endswith('.csv') or file.endswith('.ods')):
             # Construct full paths for source and destination            
             source_file = os.path.join(source_folder, file)
             destination_file = os.path.join(destination_folder, file)
@@ -22,11 +23,11 @@ def move_csv_files(source_folder, destination_folder):
             # Move the file to the destination folde
             try:
                 shutil.move(source_file, destination_file)
-                #print(f"[O] Moving: {file} -> {destination_folder}")
-                print(f"\033[92m[O] Moving: {file} -> {destination_folder}\033[0m")  # Green color for success
+                print(f"[O] Moving: {file} -> {destination_folder}")
+                #print(f"\033[92m[O] Moving: {file} -> {destination_folder}\033[0m")  # Green color for success
             except PermissionError:
-                #print(f"[X] PermissionErr: Moving {file} ; keep retry...")
-                print(f"\033[91;1m[X] PermissionErr: Moving {file} ; keep retry...\033[0m")  # Light red color for PermissionError
+                print(f"[X] PermissionErr: Moving {file} ; keep retry...")
+                #print(f"\033[91;1m[X] PermissionErr: Moving {file} ; keep retry...\033[0m")  # Light red color for PermissionError
                 
             except Exception as e:
                 print(f"[X] Unexpected error: {e}")
@@ -44,7 +45,7 @@ def main():
     while True:
         move_csv_files(source_folder, destination_folder)
         # Check every 2 seconds (adjust as needed)
-        time.sleep(3)
+        time.sleep(2)
 
 if __name__ == "__main__":
     main()
